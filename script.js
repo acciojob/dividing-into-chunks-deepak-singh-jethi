bcff7b7d30a9b6d4653ce6f2a58c809e4a64dd06
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-26 00:20:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  const result = [];
+  let currentSubarray = [];
+
+  for (const num of arr) {
+    // Check if adding the current number exceeds the maximum sum
+    if (currentSubarray.reduce((sum, x) => sum + x, 0) + num > n) {
+      // Start a new subarray
+      result.push(currentSubarray);
+      currentSubarray = [];
+    }
+    currentSubarray.push(num);
+  }
+
+  // Add the last subarray, if any
+  if (currentSubarray.length > 0) {
+    result.push(currentSubarray);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
